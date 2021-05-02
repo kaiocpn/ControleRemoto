dda05422c89748883738b4dea0c499978c925c46
CASA <CASA@DESKTOP-HQ1FO1N>
2021-05-02 15:37:12 -0300

    Controle

---
+++ Estudo06/src/Aula06.java
@@ -0,0 +1,17 @@
+
+public class Aula06 {
+
+	public static void main(String[] args) {
+		ControleRemoto c = new ControleRemoto ();
+		
+		
+		c.ligar();
+		
+		
+						
+		c.abriMenu();	
+		
+
+	}
+
+}
